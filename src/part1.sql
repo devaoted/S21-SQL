2a598bd4234d5ab1916d149409cf4ae42a884003
Shanda Wilmer <swilmer@student.21-school.ru>
2023-05-23 19:04:19 +0200

    ch_p2p, problem importing p2p csv

--- src/part1.sql
+++ src/part1.sql
@@ -1,7 +1,7 @@
 -- CREATE TYPE status AS ENUM ('start', 'success', 'failure');
 
 -- Для пересоздания базы, раскомменчиваем и запускаем всё.
--- DROP TABLE peers, tasks, p2p, verter, checks, transferred_points, friends, recommendations, xp, time_tracking;
+DROP TABLE IF EXISTS peers, tasks, p2p, verter, checks, transferred_points, friends, recommendations, xp, time_tracking;
 
 -- Таблицы
 CREATE TABLE IF NOT EXISTS peers (
@@ -29,12 +29,40 @@ CREATE TABLE IF NOT EXISTS verter (
     time time
 );
 
+CREATE OR REPLACE FUNCTION ch_p2p (
+    p_check_id bigint,
+    p_checking_peer text,
+    p_state status
+)
+RETURNS BOOLEAN
+AS $$
+BEGIN
+    IF (
+        WITH checked AS (
+            SELECT peer, task FROM checks WHERE id = p_check_id
+        )
+        SELECT (
+            SELECT COUNT(*)
+            FROM p2p JOIN checks ON p2p.check_id = checks.id
+                INNER JOIN checked ON checks.peer = checked.peer
+                    AND checks.task = checked.task
+            WHERE p2p.checking_peer = p_checking_peer
+        ) % 2 = 0
+    ) THEN
+        RETURN p_state = 'start';
+    ELSE
+        RETURN p_state != 'start';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE TABLE IF NOT EXISTS p2p (
     id serial PRIMARY KEY,
     check_id bigint REFERENCES checks,
     checking_peer text REFERENCES peers,
     state status,
-    time time
+    time time,
+    CHECK (ch_p2p(check_id, checking_peer, state))
 );
 
 CREATE TABLE IF NOT EXISTS transferred_points (
@@ -110,7 +138,25 @@ CALL import_csv('time_tracking', 'timetrack.csv', ';');
 CALL import_csv('transferred_points', 'transfer.csv', ';');
 CALL import_csv('verter', 'verter.csv', ';');
 CALL import_csv('xp', 'xp.csv', ';');
-CALL import_csv('p2p', 'p2p.csv', ';');
+-- CALL import_csv('p2p', 'p2p.csv', ';');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (1, 'bread', 'start', '15:00:00');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (1, 'bread', 'success', '15:30:00');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (2, 'bread', 'start', '15:00:00');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (2, 'bread', 'success', '15:30:00');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (3, 'bread', 'start', '15:00:00');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (3, 'bread', 'success', '15:30:00');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (4, 'pizza', 'start', '15:00:00');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (4, 'pizza', 'success', '15:20:00');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (5, 'puzzle', 'start', '15:30:00');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (5, 'puzzle', 'success', '15:50:00');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (6, 'gifts', 'start', '16:00:00');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (6, 'gifts', 'success', '16:25:00');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (7, 'monster', 'start', '16:30:00');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (7, 'monster', 'failure', '16:45:00');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (8, 'toster', 'start', '15:00:00');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (8, 'toster', 'success', '15:20:00');
+INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (9, 'monster', 'start', '15:20:00');
+
 
 SELECT SETVAL('checks_id_seq', (SELECT MAX(id) FROM checks));
 SELECT SETVAL('p2p_id_seq', (SELECT MAX(id) FROM p2p));
