0f10195a4f760c6d1029b2e9c56ed9f3208d10a1
Shanda Wilmer <swilmer@student.21-school.ru>
2023-05-24 16:40:10 +0200

    part1 complete csv imports and constraint conflicts solved script deletes everything that was created in the script

--- src/part1.sql
+++ src/part1.sql
@@ -1,7 +1,8 @@
--- CREATE TYPE status AS ENUM ('start', 'success', 'failure');
-
 -- Для пересоздания базы, раскомменчиваем и запускаем всё.
 DROP TABLE IF EXISTS peers, tasks, p2p, verter, checks, transferred_points, friends, recommendations, xp, time_tracking;
+DROP PROCEDURE IF EXISTS import_csv(text, text, char), export_csv(text, text, char);
+DROP FUNCTION IF EXISTS checks_status(bigint), ch_checks(bigint, text, text), ch_p2p(bigint, bigint, text, status), ch_verter(bigint), ch_xp(bigint, float), ch_time_tracking(bigint, date, text, int);
+DROP TYPE IF EXISTS status;
 
 -- Таблицы
 CREATE TABLE IF NOT EXISTS peers (
@@ -15,6 +16,115 @@ CREATE TABLE IF NOT EXISTS tasks (
     max_xp float
 );
 
+CREATE TABLE IF NOT EXISTS checks (
+    id serial PRIMARY KEY,
+    peer text REFERENCES peers,
+    task text REFERENCES tasks,
+    date date
+);
+
+CREATE TYPE status AS ENUM ('start', 'success', 'failure');
+
+CREATE TABLE IF NOT EXISTS p2p (
+    id serial PRIMARY KEY,
+    check_id bigint REFERENCES checks,
+    checking_peer text REFERENCES peers,
+    state status,
+    time time
+);
+
+CREATE TABLE IF NOT EXISTS verter (
+    id serial PRIMARY KEY,
+    check_id bigint REFERENCES checks,
+    state status,
+    time time
+);
+
+CREATE TABLE IF NOT EXISTS transferred_points (
+    id serial PRIMARY KEY,
+    checking_peer text REFERENCES peers,
+    checked_peer text REFERENCES peers,
+    points_amount int
+);
+
+CREATE TABLE IF NOT EXISTS friends (
+    id serial PRIMARY KEY,
+    peer1 text REFERENCES peers,
+    peer2 text REFERENCES peers
+);
+
+CREATE TABLE IF NOT EXISTS recommendations (
+    id serial PRIMARY KEY,
+    peer text REFERENCES peers,
+    recommended_peer text REFERENCES peers
+);
+
+CREATE TABLE IF NOT EXISTS xp (
+    id serial PRIMARY KEY,
+    check_id bigint REFERENCES checks,
+    xp_amount float
+);
+
+CREATE TABLE IF NOT EXISTS time_tracking (
+    id serial PRIMARY KEY,
+    peer text REFERENCES peers,
+    date date,
+    time time,
+    state int
+);
+
+-- Процедуры импорта и экспорта CSV
+CREATE OR REPLACE PROCEDURE import_csv(
+    IN table_name text,
+    IN csv_file text,
+    IN delimiter char DEFAULT ';'
+) AS 
+    $$
+    DECLARE
+        data_path text := '/Users/konstantin.d/Library/Mobile Documents/com~apple~CloudDocs/School21/SQL Info21 v1.0/swilmer/datasets/';
+    BEGIN
+        EXECUTE format('COPY %s FROM ''%s'' DELIMITER ''%s'' CSV HEADER NULL AS ''null'';', table_name, data_path || csv_file, delimiter);
+    END;
+    $$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE PROCEDURE export_csv(
+    IN table_name text,
+    IN csv_file text,
+    IN delimiter char DEFAULT ';'
+) AS 
+    $$
+    DECLARE
+        data_path text := '/Users/vladislavepanesnikov/Desktop/programming/school21/sber/sql/sql2_info21/datasets/';
+    BEGIN
+        EXECUTE format('COPY %s TO ''%s'' DELIMITER ''%s'' CSV HEADER NULL AS ''null'';', table_name, data_path || csv_file, delimiter);
+    END;
+    $$
+LANGUAGE plpgsql;
+
+-- Создание базы данных
+SET datestyle = dmy;
+CALL import_csv('peers', 'peers.csv', ';');
+CALL import_csv('tasks', 'tasks.csv', ';');
+CALL import_csv('checks', 'checks.csv', ';');
+CALL import_csv('p2p', 'p2p.csv', ';');
+CALL import_csv('verter', 'verter.csv', ';');
+CALL import_csv('transferred_points', 'transfer.csv', ';');
+CALL import_csv('friends', 'friends.csv', ';');
+CALL import_csv('recommendations', 'recommends.csv', ';');
+CALL import_csv('xp', 'xp.csv', ';');
+CALL import_csv('time_tracking', 'timetrack.csv', ';');
+
+SELECT SETVAL('checks_id_seq', (SELECT MAX(id) FROM checks));
+SELECT SETVAL('p2p_id_seq', (SELECT MAX(id) FROM p2p));
+SELECT SETVAL('verter_id_seq', (SELECT MAX(id) FROM verter));
+SELECT SETVAL('transferred_points_id_seq', (SELECT MAX(id) FROM transferred_points));
+SELECT SETVAL('friends_id_seq', (SELECT MAX(id) FROM friends));
+SELECT SETVAL('recommendations_id_seq', (SELECT MAX(id) FROM recommendations));
+SELECT SETVAL('xp_id_seq', (SELECT MAX(id) FROM xp));
+SELECT SETVAL('time_tracking_id_seq', (SELECT MAX(id) FROM time_tracking));
+
+-- Функции проверок
 CREATE OR REPLACE FUNCTION checks_status (
     p_check_id bigint
 )
@@ -57,35 +167,8 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TABLE IF NOT EXISTS checks (
-    id serial PRIMARY KEY,
-    peer text REFERENCES peers,
-    task text REFERENCES tasks,
-    date date,
-    CHECK (ch_checks(id, peer, task))
-);
-
-CREATE OR REPLACE FUNCTION ch_verter (
-    p_check_id bigint
-)
-RETURNS BOOLEAN
-AS $$
-BEGIN
-    RETURN EXISTS (
-        SELECT * FROM p2p WHERE check_id = p_check_id AND state = 'success'
-    );
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TABLE IF NOT EXISTS verter (
-    id serial PRIMARY KEY,
-    check_id bigint REFERENCES checks,
-    state status,
-    time time,
-    CHECK (ch_verter(check_id))
-);
-
 CREATE OR REPLACE FUNCTION ch_p2p (
+    p_id bigint,
     p_check_id bigint,
     p_checking_peer text,
     p_state status
@@ -103,6 +186,7 @@ BEGIN
                 INNER JOIN checked ON checks.peer = checked.peer
                     AND checks.task = checked.task
             WHERE p2p.checking_peer = p_checking_peer
+                AND p2p.id < p_id
         ) % 2 = 0
     ) THEN
         RETURN p_state = 'start';
@@ -112,33 +196,17 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TABLE IF NOT EXISTS p2p (
-    id serial PRIMARY KEY,
-    check_id bigint REFERENCES checks,
-    checking_peer text REFERENCES peers,
-    state status,
-    time time,
-    CHECK (ch_p2p(check_id, checking_peer, state))
-);
-
-CREATE TABLE IF NOT EXISTS transferred_points (
-    id serial PRIMARY KEY,
-    checking_peer text REFERENCES peers,
-    checked_peer text REFERENCES peers,
-    points_amount int
-);
-
-CREATE TABLE IF NOT EXISTS friends (
-    id serial PRIMARY KEY,
-    peer1 text REFERENCES peers,
-    peer2 text REFERENCES peers
-);
-
-CREATE TABLE IF NOT EXISTS recommendations (
-    id serial PRIMARY KEY,
-    peer text REFERENCES peers,
-    recommended_peer text REFERENCES peers
-);
+CREATE OR REPLACE FUNCTION ch_verter (
+    p_check_id bigint
+)
+RETURNS BOOLEAN
+AS $$
+BEGIN
+    RETURN EXISTS (
+        SELECT * FROM p2p WHERE check_id = p_check_id AND state = 'success'
+    );
+END;
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION ch_xp (
     p_check_id bigint,
@@ -155,14 +223,8 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TABLE IF NOT EXISTS xp (
-    id serial PRIMARY KEY,
-    check_id bigint REFERENCES checks,
-    xp_amount float,
-    CHECK (ch_xp(check_id, xp_amount))
-);
-
 CREATE OR REPLACE FUNCTION ch_time_tracking (
+    p_id bigint,
     p_date date,
     p_peer text,
     p_state int
@@ -173,13 +235,14 @@ DECLARE
     v_date date;
 BEGIN
     v_date := (
-        SELECT date FROM time_tracking ORDER BY id DESC LIMIT 1
+        SELECT date FROM time_tracking WHERE id < p_id ORDER BY id DESC LIMIT 1
     );
     IF (
         v_date = p_date
     ) THEN
         RETURN (
-            SELECT (SELECT state FROM time_tracking WHERE date = p_date AND peer = p_peer
+            SELECT (SELECT state FROM time_tracking
+                WHERE id < p_id AND date = p_date AND peer = p_peer
                 ORDER BY id DESC LIMIT 1) != p_state
         );
     ELSE
@@ -195,112 +258,9 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TABLE IF NOT EXISTS time_tracking (
-    id serial PRIMARY KEY,
-    peer text REFERENCES peers,
-    date date,
-    time time,
-    state int,
-    CHECK (ch_time_tracking(date, peer, state))
-);
-
--- Процедуры импорта и экспорта CSV
-CREATE OR REPLACE PROCEDURE import_csv(
-    IN table_name text,
-    IN csv_file text,
-    IN delimiter char DEFAULT ';'
-) AS 
-    $$
-    DECLARE
-        data_path text := '/Users/vladislavepanesnikov/Desktop/programming/school21/sber/sql/sql2_info21/datasets/';
-    BEGIN
-        EXECUTE format('COPY %s FROM ''%s'' DELIMITER ''%s'' CSV HEADER NULL AS ''null'';', table_name, data_path || csv_file, delimiter);
-    END;
-    $$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE PROCEDURE export_csv(
-    IN table_name text,
-    IN csv_file text,
-    IN delimiter char DEFAULT ';'
-) AS 
-    $$
-    DECLARE
-        data_path text := '/Users/vladislavepanesnikov/Desktop/programming/school21/sber/sql/sql2_info21/datasets/';
-    BEGIN
-        EXECUTE format('COPY %s TO ''%s'' DELIMITER ''%s'' CSV HEADER NULL AS ''null'';', table_name, data_path || csv_file, delimiter);
-    END;
-    $$
-LANGUAGE plpgsql;
-
--- Создание базы данных
-SET datestyle = dmy;
-CALL import_csv('peers', 'peers.csv', ';');
-CALL import_csv('tasks', 'tasks.csv', ';');
-CALL import_csv('friends', 'friends.csv', ';');
-CALL import_csv('recommendations', 'recommends.csv', ';');
-CALL import_csv('transferred_points', 'transfer.csv', ';');
--- CALL import_csv('checks', 'checks.csv', ';');
--- CALL import_csv('p2p', 'p2p.csv', ';');
--- CALL import_csv('verter', 'verter.csv', ';');
--- CALL import_csv('time_tracking', 'timetrack.csv', ';');
-
-INSERT INTO checks (peer, task, date) VALUES ('toster', 'C2_SimpleBashUtils', '2023-04-19');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (1, 'bread', 'start', '15:00:00');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (1, 'bread', 'success', '15:30:00');
-INSERT INTO verter (check_id, state, time) VALUES (1, 'start', '15:30:00');
-INSERT INTO verter (check_id, state, time) VALUES (1, 'success', '15:35:00');
-
-INSERT INTO checks (peer, task, date) VALUES ('toster', 'C3_s21_string+', '2023-04-20');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (2, 'bread', 'start', '15:00:00');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (2, 'bread', 'success', '15:30:00');
-
-INSERT INTO checks (peer, task, date) VALUES ('toster', 'C4_s21_decimal', '2023-04-21');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (3, 'bread', 'start', '15:00:00');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (3, 'bread', 'success', '15:30:00');
-
-INSERT INTO checks (peer, task, date) VALUES ('puzzle', 'C2_SimpleBashUtils', '2023-04-22');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (4, 'pizza', 'start', '15:00:00');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (4, 'pizza', 'success', '15:20:00');
-INSERT INTO verter (check_id, state, time) VALUES (4, 'start', '15:20:00');
-INSERT INTO verter (check_id, state, time) VALUES (4, 'success', '15:25:00');
-
-INSERT INTO checks (peer, task, date) VALUES ('pizza', 'C2_SimpleBashUtils', '2023-04-22');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (5, 'puzzle', 'start', '15:30:00');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (5, 'puzzle', 'success', '15:50:00');
-INSERT INTO verter (check_id, state, time) VALUES (5, 'start', '15:50:00');
-INSERT INTO verter (check_id, state, time) VALUES (5, 'failure', '15:55:00');
-
-INSERT INTO checks (peer, task, date) VALUES ('puzzle', 'C3_s21_string+', '2023-04-22');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (6, 'toster', 'start', '16:00:00');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (6, 'toster', 'success', '16:25:00');
-
-INSERT INTO checks (peer, task, date) VALUES ('puzzle', 'C4_s21_decimal', '2023-04-22');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (7, 'toster', 'start', '16:30:00');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (7, 'toster', 'failure', '16:45:00');
-
-INSERT INTO checks (peer, task, date) VALUES ('pizza', 'C2_SimpleBashUtils', '2023-04-23');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (8, 'toster', 'start', '15:00:00');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (8, 'toster', 'success', '15:20:00');
-INSERT INTO verter (check_id, state, time) VALUES (8, 'start', '15:20:00');
-
-INSERT INTO checks (peer, task, date) VALUES ('puzzle', 'C4_s21_decimal', '2023-04-23');
-INSERT INTO p2p (check_id, checking_peer, state, time) VALUES (9, 'bread', 'start', '15:20:00');
-
-CALL import_csv('xp', 'xp.csv', ';');
-
-INSERT INTO time_tracking (peer, date, time, state) VALUES ('toster', '2023-04-22', '13:37:00', 1);
-INSERT INTO time_tracking (peer, date, time, state) VALUES ('toster', '2023-04-22', '15:48:00', 2);
-INSERT INTO time_tracking (peer, date, time, state) VALUES ('toster', '2023-04-22', '16:02:00', 1);
-INSERT INTO time_tracking (peer, date, time, state) VALUES ('toster', '2023-04-22', '20:00:00', 2);
-INSERT INTO time_tracking (peer, date, time, state) VALUES ('monster', '2023-04-22', '21:00:00', 1);
-INSERT INTO time_tracking (peer, date, time, state) VALUES ('monster', '2023-04-22', '22:00:00', 2);
-
--- SELECT SETVAL('checks_id_seq', (SELECT MAX(id) FROM checks));
--- SELECT SETVAL('p2p_id_seq', (SELECT MAX(id) FROM p2p));
--- SELECT SETVAL('verter_id_seq', (SELECT MAX(id) FROM verter));
--- SELECT SETVAL('time_tracking_id_seq', (SELECT MAX(id) FROM time_tracking));
-SELECT SETVAL('transferred_points_id_seq', (SELECT MAX(id) FROM transferred_points));
-SELECT SETVAL('xp_id_seq', (SELECT MAX(id) FROM xp));
-SELECT SETVAL('friends_id_seq', (SELECT MAX(id) FROM friends));
-SELECT SETVAL('recommendations_id_seq', (SELECT MAX(id) FROM recommendations));
+-- Добавление проверок
+ALTER TABLE checks ADD CHECK (ch_checks(id, peer, task));
+ALTER TABLE verter ADD CHECK (ch_verter(check_id));
+ALTER TABLE p2p ADD CHECK (ch_p2p(id, check_id, checking_peer, state));
+ALTER TABLE xp ADD CHECK (ch_xp(check_id, xp_amount));
+ALTER TABLE time_tracking ADD CHECK (ch_time_tracking(id, date, peer, state));
