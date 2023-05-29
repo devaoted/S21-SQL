982ba0b41b231fb098781fdb38d58d41c61e23c7
Vladislav Epaneshnikov <v.epaneshnikov@inbox.ru>
2023-05-29 23:08:24 +0300

    changed checks.csv, p2p.csv for test3.6.sql

--- src/tests/test3.6.sql
+++ src/tests/test3.6.sql
@@ -1,5 +1,3 @@
--- CALL add_p2p('bread', 'CPP1_s21_matrix+', 'toster', 'start', '18:00');
--- CALL add_p2p('bread', 'CPP1_s21_matrix+', 'toster', 'success', '18:30');
--- SELECT date, task, COUNT(*) FROM checks GROUP BY date, task;
+SELECT date, task, COUNT(*) FROM checks GROUP BY date, task;
 
 SELECT * FROM get_most_frequent_tasks();
