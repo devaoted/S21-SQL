91beab605514cc6aa4ae595be0abb6994d3d5812
Vladislav Epaneshnikov <v.epaneshnikov@inbox.ru>
2023-05-29 22:02:52 +0300

    added test3.1/changed friends dataset

--- src/part3.sql
+++ src/part3.sql
@@ -7,7 +7,7 @@ BEGIN
     SELECT tp1.checking_peer AS peer1, tp1.checked_peer AS peer2, tp1.points_amount - COALESCE(tp2.points_amount, 0) AS number
     FROM transferred_points tp1
     LEFT JOIN transferred_points tp2 ON tp1.checked_peer = tp2.checking_peer AND tp1.checking_peer = tp2.checked_peer
-     WHERE tp1.checking_peer < tp1.checked_peer
+    WHERE tp1.checking_peer < tp1.checked_peer
         OR (tp1.checking_peer > tp1.checked_peer AND tp2.checked_peer is NULL);
 END;
 $$ LANGUAGE plpgsql;
@@ -19,7 +19,7 @@ CREATE OR REPLACE FUNCTION get_xp()
 RETURNS TABLE (Peer text, Task text, XP float) AS $$
 BEGIN
     RETURN QUERY
-    SELECT checks.peer, checks.task, xp_amount FROM xp JOIN checks ON check_id = checks.id
+    SELECT checks.peer, checks.task, xp_amount FROM xp JOIN checks ON check_id = checks.id;
     -- WHERE checks_status(checks.id) = 'success';
 END;
 $$ LANGUAGE plpgsql;
@@ -93,7 +93,7 @@ BEGIN
         GROUP BY date
     ) mc
         ON tc.date = mc.date AND tc.check_count = mc.max_count
-    ORDER BY 1 DESC, 2 ASC
+    ORDER BY 1 DESC, 2 ASC;
 END;
 $$ LANGUAGE plpgsql;
 
---
+++ src/tests/test3.1.sql
@@ -0,0 +1,2 @@
+SELECT * FROM transferred_points;
+SELECT * FROM get_transferred_points();
